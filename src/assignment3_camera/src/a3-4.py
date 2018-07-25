8c2ba129632291bf8d092cf6277fda153d5b5d66
apinz <pinz@zedat.fu-berlin.de>
2018-07-25 14:20:21 +0200

    dummys for a3-4 to a3-6

---
+++ src/assignment3_camera/src/a3-4.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env python
+import roslib
+import sys
+import rospy
+import cv2
+from std_msgs.msg import String
+from geometry_msgs.msg import Pose
+from geometry_msgs.msg import Point
+from geometry_msgs.msg import Quaternion
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+class image_converter:
+
+  def __init__(self):
+    self.image_pub = rospy.Publisher("/image_processing/bin_img", Image, queue_size=1)
+
+    self.bridge = CvBridge()
+    self.image_sub = rospy.Subscriber("/app/camera/rgb/image_raw", Image, self.callback, queue_size=1)
+
+
+  def callback(self,data):
+    try:
+      cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+    except CvBridgeError as e:
+      print(e)
+
+    #make it gray
+    gray = cv2.cvtColor(cv_image, cv2.COLOR_BGR2GRAY)
+    
+    #bi_gray
+    bi_gray_max = 255
+    bi_gray_min = 245
+    ret, thresh_img = cv2.threshold(gray, bi_gray_min, bi_gray_max, cv2.THRESH_BINARY)
+
+    for x in range(thresh_img.shape[0]):
+        for y in range(thresh_img.shape[1]):
+            #TODO: find white points
+
+    cv2.imshow("Image window", gray)
+    #~ cv2.imwrite('grayscale.png', gray)
+    cv2.waitKey(3)
+    
+    try:
+      self.image_pub.publish(self.bridge.cv2_to_imgmsg(gray, "mono8"))
+    except CvBridgeError as e:
+      print(e) 
+
+def main(args):
+    print("Running\n")
+    rospy.init_node('image_converter', anonymous=True)
+    ic = image_converter()
+    try:
+        rospy.spin()
+    except KeyboardInterrupt:
+        print("Shutting down")
+    cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    main(sys.argv)
