1f0aa43a7feb657ec5df966de499e00a3ed55eb8
apinz <pinz@zedat.fu-berlin.de>
2018-07-27 13:54:54 +0200

    a4-3 dummy

---
+++ src/assignment4_line_detection/src/a4-3.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+import numpy as np
+import sys
+
+def ransac(img):
+    # TODO
+    return
+
+def main(args):
+    # Use RANSAC to estimate a linear model (y = mx+b) on the image and obtain the two equations of the lines on the road.
+    filtered_hsv = 0
+    ransac(filtered_hsv)
+
+if __name__ == '__main__':
+    main(sys.argv)
