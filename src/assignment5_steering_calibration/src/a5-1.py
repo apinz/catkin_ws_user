c5c8b0db693b98779a843693f9fde7423c5574f9
apinz <pinz@zedat.fu-berlin.de>
2018-07-31 15:04:16 +0200

    a5-1

--- src/assignment5_steering_calibration/src/a5-1.py
+++ src/assignment5_steering_calibration/src/a5-1.py
@@ -3,26 +3,26 @@
 # --- imports ---
 import rospy
 import numpy as np
+from collections import deque
 from nav_msgs.msg import OccupancyGrid
 from sensor_msgs.msg import LaserScan
 
-METER_TO_PIXEL_RATIO = 10
 MAX_RANGE = 8
 MIN_RANGE = 0.15
-COORDINATE_OFFSET = MAX_RANGE * METER_TO_PIXEL_RATIO
-GRID_WIDTH = 2 * COORDINATE_OFFSET
-GRID_HEIGHT = 2 * COORDINATE_OFFSET
+GRID_RESOLUTION = 0.05
+GRID_WIDTH = int(MAX_RANGE / GRID_RESOLUTION)
+GRID_HEIGHT = GRID_WIDTH
+BUFFER_SIZE = 1 * 360
 
-# --- definitions ---
+# set all values to "FREE"
 def resetGrid():
     global occupancy_grid
-    
-    # set all values to "FREE"
+    occupancy_grid.data = np.zeros(occupancy_grid.info.width * occupancy_grid.info.height)
 
 # to a given cartesian x,y coordinate, mark the corresponding cell in the grid as "OCCUPIED"
 def setCell(x,y):
     global occupancy_grid
-
+    
     res = occupancy_grid.info.resolution
     x_scaled = (x * 1.0 / res) + occupancy_grid.info.width/2
     y_scaled = (y * 1.0 / res) + occupancy_grid.info.height/2
@@ -37,24 +37,16 @@ def setCell(x,y):
 # convert scan measurements into an occupancy grid
 def scanCallback(scan_msg):
     global occupancy_grid
-
+    global occupied_cells
+    
     resetGrid()
     
     for i in range(0, len(scan_msg.ranges)):
-        if(scan_msg.ranges[i] == np.inf):
-            x = COORDINATE_OFFSET + int(round(MAX_RANGE * np.cos(i) * METER_TO_PIXEL_RATIO))
-            y = COORDINATE_OFFSET + int(round(MAX_RANGE * np.sin(i) * METER_TO_PIXEL_RATIO))
-        elif(scan_msg.ranges[i] == -np.inf):
-            x = COORDINATE_OFFSET + int(round(MIN_RANGE * np.cos(i) * METER_TO_PIXEL_RATIO))
-            y = COORDINATE_OFFSET + int(round(MIN_RANGE * np.sin(i) * METER_TO_PIXEL_RATIO))
-        else:
-            x = COORDINATE_OFFSET + int(round(scan_msg.ranges[i] * np.cos(i) * METER_TO_PIXEL_RATIO))
-            y = COORDINATE_OFFSET + int(round(scan_msg.ranges[i] * np.sin(i) * METER_TO_PIXEL_RATIO))
-            
-        if(y != 0):
-            occupancy_grid.data[(y - 1) * occupancy_grid.info.width + x] = scan_msg.intensities[i]
-        else:
-            occupancy_grid.data[x] = scan_msg.intensities[i]
+        if(scan_msg.ranges[i] >= scan_msg.range_min and scan_msg.ranges[i] <= scan_msg.range_max):
+            occupied_cells.append(np.array([scan_msg.ranges[i] * np.cos(i), scan_msg.ranges[i] * np.sin(i)]))
+    
+    for i in occupied_cells:
+        setCell(i[0],i[1])
     
     pub_grid.publish(occupancy_grid)
 
@@ -62,10 +54,13 @@ def scanCallback(scan_msg):
 # --- main ---
 rospy.init_node("scan_grid")
 
+# use deque as ringbuffer for occupied cells so we can keep a history of the laser scan measures
+occupied_cells = deque(np.full((BUFFER_SIZE,2), np.inf), maxlen = BUFFER_SIZE)
+
 # init occupancy grid
 occupancy_grid = OccupancyGrid()
 occupancy_grid.header.frame_id = "laser"
-occupancy_grid.info.resolution = 1 # in m/cell
+occupancy_grid.info.resolution = GRID_RESOLUTION # in m/cell
 
 # width x height cells3
 occupancy_grid.info.width = GRID_WIDTH
