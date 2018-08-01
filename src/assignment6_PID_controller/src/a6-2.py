39c0b4535abed82a6e0438632e64751fd7cb30fd
apinz <pinz@zedat.fu-berlin.de>
2018-08-01 16:24:18 +0200

    a6-2

--- src/assignment6_PID_controller/src/a6-2.py
+++ src/assignment6_PID_controller/src/a6-2.py
@@ -9,25 +9,26 @@ from std_msgs.msg import Float64
 class SetpointStateNode:
 
     def __init__(self):
-        self.setpoint_pub = rospy.Publisher("/setpoint/data", Float64, queue_size=1)
-        self.state_pub = rospy.Publisher("/state/data", Float64, queue_size=1)
-        self.control_sub = rospy.Subscriber("/control_effort/data", Float64, self.callback, queue_size=1)
-        self.yaw_sub = rospy.Subscriber("/yaw", Float32, self.callback, queue_size=1)
+        self.setpoint_pub = rospy.Publisher("/setpoint", Float64, queue_size=1)
+        self.state_pub = rospy.Publisher("/state", Float64, queue_size=1)
+        self.control_sub = rospy.Subscriber("/control_effort", Float64, self.controlCallback, queue_size=1)
+        self.yaw_sub = rospy.Subscriber("/Kaenel/yaw", Float32, self.yawCallback, queue_size=1)
 
-    def yawCallback(self,data):
-        self.state_pub.publish(data)
+    def yawCallback(self, data):
+        self.state_pub.publish(Float64(data.data))
+        self.setSetpoint(0)
 
-    def controlCallback(self,data):
-        # TODO: Control the car
+    def controlCallback(self, data):
+        print(str(data))
+        
+    def setSetpoint(self, setpoint):
+        self.setpoint_pub.publish(Float64(setpoint))
 
 def main(args):
     print("Running\n")
     rospy.init_node('PID_setpoint_state_node', anonymous=True)
     ssn = SetpointStateNode()
-    try:
-        rospy.spin()
-    except KeyboardInterrupt:
-        print("Shutting down")
+    rospy.spin()
 
 if __name__ == '__main__':
     main(sys.argv)
