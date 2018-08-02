d8fb2aba7b98bd02f5c20ac6f28b6d0c31cf3071
Tim <tim.v.kaenel@web.de>
2018-08-02 17:41:53 +0200

    new vector fields

--- src/assignment7_vector_field/src/control.py
+++ src/assignment7_vector_field/src/control.py
@@ -7,32 +7,36 @@ from geometry_msgs.msg import PoseWithCovarianceStamped, PointStamped
 from tf.transformations import euler_from_quaternion, quaternion_from_euler
 from std_msgs.msg import Int16, UInt8
 
-PATH = "~/Schreibtisch/uni/Robotik/catkin_ws_user/src/fub_navigation/src/"
+PATH = "/home/coder/Schreibtisch/uni/Robotik/catkin_ws_user/src/assignment7_vector_field/src/"
+NAMESPACE = "/AljoschaTim"
+SPEED = 500
 
 class ForceController:
     def __init__(self):
-        self.pub = rospy.Publisher("/AljoschaTim/steering", UInt8, queue_size = 1)
-        self.pub_speed = rospy.Publisher("/AljoschaTim/speed", Int16, queue_size = 100, latch=True)
+        self.pub_steering = rospy.Publisher(NAMESPACE + "/steering", UInt8, queue_size = 1)
+        self.pub_speed = rospy.Publisher(NAMESPACE + "/speed", Int16, queue_size = 100, latch=True)
         #self.sub_yaw = rospy.Subscriber("/model_car/yaw", Float32, self.callback, queue_size=1)
         self.sub_odom = rospy.Subscriber("/localization/odom/1", Odometry, self.callback, queue_size = 1)
         self.sub_points = rospy.Subscriber("/clicked_point", PointStamped, self.lane_callback, queue_size = 1)
-
+	self.shutdown_ = False
         self.map_size_x = 600 #cm
         self.map_size_y = 400 #cm
         self.resolution = 10 # cm
         self.lane = 1
         if (self.lane == 1):
-        	self.matrix = np.load(PATH + "matrix100cm_lane1.npy")
+            self.matrix = np.load(PATH + "matrix100cm_lane1.npy")
         else:
-        	self.matrix = np.load(PATH + "matrix100cm_lane2.npy")
+       	    self.matrix = np.load(PATH + "matrix100cm_lane2.npy")
+	#Fuege eventListener hinzu, wird gecalled wenn sich rospy beendet, Funktion setzt Speed des Fahrzeuges auf 0
+	rospy.on_shutdown(self.shutdown)
 
     def lane_callback(self, data):
     	if (self.lane == 1):
-    		self.lane = 2
-    		self.matrix = np.load(PATH + "matrix100cm_lane1.npy")
+    	    self.lane = 2
+    	    self.matrix = np.load(PATH + "matrix100cm_lane1.npy")
     	else:
-    		self.lane = 1
-    		self.matrix = np.load(PATH + "matrix100cm_lane2.npy")
+    	    self.lane = 1
+    	    self.matrix = np.load(PATH + "matrix100cm_lane2.npy")
 
     def callback(self, data):
 
@@ -64,9 +68,9 @@ class ForceController:
         steering=Kp*np.arctan(f_y/(2.5*f_x))
 
         if (f_x > 0):
-            speed = 150
+            speed = SPEED
         else:
-            speed = -150 
+            speed = -SPEED 
             if (f_y > 0):
             	steering = -np.pi/2
             if (f_y < 0):
@@ -77,20 +81,26 @@ class ForceController:
 
         if (steering < -(np.pi)/2):
             steering = -(np.pi)/2
-
+	
 
         steering = 90 + steering * (180/np.pi)
         print("Steering: " + str(steering))
-        self.pub.publish(UInt8(steering))
-        self.pub_speed.publish(Int16(speed))
+        self.pub_steering.publish(UInt8(steering))
+	if not self.shutdown_:
+	    #speed
+	    print("publishing speed")
+	    self.pub_speed.publish(Int16(speed))
 
-
-def log(name, var):
-    print '%s:\n%s\n' % (name, var)
-
+    #ein Listener wir bei der init() erstellt, dieser called die shutdown Funktion, wenn das script beendet wird. 
+    #Setzt den Speed des autos auf 0
+    def shutdown(self):
+        print("shutdown!")
+        self.shutdown_=True
+        self.pub_speed.publish(Int16(0))
+	rospy.sleep(1)
 
 def main():
-    rospy.init_node('ForceController')
+    rospy.init_node('AljoschaTimRacer')
     ForceController()  # constructor creates publishers / subscribers
     rospy.spin()
 
