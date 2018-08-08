1e19f5b5d38bbc220e7634dfae08f50343b6aed9
apinz <pinz@zedat.fu-berlin.de>
2018-08-08 14:01:02 +0200

    a9 dummy

---
+++ src/assignment9_camera_localization/src/a9.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python2
+import rospy
+import roslib
+import sys
+import math
+import numpy as np
+import cv2
+
+from sensor_msgs.msg import Image
+from cv_bridge import CvBridge, CvBridgeError
+
+PATH = "/root/catkin_ws_user/src/assignment9_camera_localization/src/"
+
+class CameraLocalization:
+    def __init__(self):
+        self.depth_image_sub = rospy.Subscriber("/AljoschaTim/app/camera/depth/image_raw", Image, self.depthImageCallback, queue_size = 1)
+        self.ir_image_sub = rospy.Subscriber("/AljoschaTim/app/camera/ir/image_raw", Image, self.irImageCallback, queue_size = 1)
+    
+    def depthImageCallback(self, depthData):
+        print(str(depthData))
+        depthImage = self.bridge.imgmsg_to_cv2(depthData, "16UC1")
+        depthArray = np.array(depthImage, dtype=np.uint16)
+        cv2.imwrite("depth_img.png", depthArray)
+        
+    def irImageCallback(self, irData):
+        print(str(irData))
+
+def main():
+    print("Running\n")
+    rospy.init_node('camera_localization')
+    rospy.spin()
+
+if __name__ == '__main__':
+    main()
